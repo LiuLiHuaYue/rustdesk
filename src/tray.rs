0fd12c686637312c79b783e54123fbb74f0d1047
LiuLiHuaYue <rushmoran@foxmail.com>
2025-03-28 19:18:44 +0800

    init

--- src/tray.rs
+++ src/tray.rs
@@ -9,7 +9,7 @@ use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 pub fn start_tray() {
-    if crate::ui_interface::get_builtin_option(hbb_common::config::keys::OPTION_HIDE_TRAY) == "N" {
+    if crate::ui_interface::get_builtin_option(hbb_common::config::keys::OPTION_HIDE_TRAY) == "Y" {
         #[cfg(target_os = "macos")]
         {
             loop {
